32a09d85a396859b80f508b490d48b996850acfe
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2016-12-11 16:27:02 +0100

    Focus on Torun

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  lat: 45.519743,
-  lng: -122.680522,
-  zoom: 10,
+  lat: 53.01375,
+  lng: 18.59814,
+  zoom: 7,
+  emberConfLocation: [45.528298, -122.662986],
+  hotel: [45.530891, -122.655504],
   options: {
     title: 'How I spend my days',
     height: 300,
