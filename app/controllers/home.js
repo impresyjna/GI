0adcc2b31ad69b2ead24c22b0d48459a48fc77dd
Asia <joanna.furmaniak@student.put.poznan.pl>
2016-12-18 15:25:44 +0100

    Cleaning up

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,22 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  lat: 53.01375,
-  lng: 18.59814,
-  zoom: 7,
-  emberConfLocation: [45.528298, -122.662986],
-  hotel: [45.530891, -122.655504],
-  options: {
-    title: 'How I spend my days',
-    height: 300,
-    width: 400,
-
-    animation: {
-      startup: true,
-      easing: 'inAndOut',
-    },
-  },
-
   actions: {
     targetButton(item) {
       //this.transitionToRoute(item);
