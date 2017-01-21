4d5bfe8e4bfb1e9914bf978c7548029551f97bc7
impresyjna <joanna.furmaniak@student.put.poznan.pl>
2017-01-21 19:48:21 +0100

    Before doom day

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,15 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  model() {
-    return [
-      ['Task', 'Hours per Day'],
-      ['Work', 11],
-      ['Eat', 2],
-      ['Commute', 2],
-      ['Watch TV', 2],
-      ['Sleep', 7],
-    ];
-  }
-
 });
