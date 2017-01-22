b7b15cccb1c94981905dbe370e70ee52063640e0
impresyjna <joanna.furmaniak@student.put.poznan.pl>
2017-01-22 01:27:41 +0100

    For debug

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+import DS from 'ember-data';
 
 let App;
 
@@ -11,6 +12,10 @@ App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
+});
+
+App.ApplicationAdapter = DS.JSONAPIAdapter.extend({
+  url: 'http://gi-kp.azurewebsites.net/'
 });
 
 loadInitializers(App, config.modulePrefix);
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  queryParams: ['levelOfData'],
+  levelOfData: 1,
+
   actions: {
     targetButton(item) {
       //this.transitionToRoute(item);
