cfe273d8778a8c7f93f1b319bbc26807cbb515c0
impresyjna <joanna.furmaniak@student.put.poznan.pl>
2017-01-26 23:38:24 +0100

    Not working action

--- app/routes/group.js
+++ app/routes/group.js
@@ -12,6 +12,17 @@ export default Ember.Route.extend({
 
   serialize: function(model) {
     return { subgroups: model.get('subgroup') };
+  },
+
+  actions: {
+    getSubgroupInfo: function(subgroup,params) {
+      console.log("SIEMA");
+      console.log(params.group_id);
+      console.log(subgroup.id);
+      console.log(params.levelOfData);
+      // var subgroupInfoForChart = Ember.$.getJSON('http://gi-kp.azurewebsites.net/groups/' + params.group_id +  '/subgroups/' + subgroup.id + '?levelOfData=' + params.levelOfData );
+      // console.log(subgroupInfoForChart);
+    }
   }
 
 
